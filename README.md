5188bb63b67b3a8b076a53c7a10ab040e16c9e32
MatthMiller <matheus.monteiro.genesis@gmail.com>
2023-11-17 15:05:36 -0300

    Documentação da classe Calculadora com Javadoc e atualização no README.md

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # 🧮 Atividade de testes automatizados
 
+Atividade da matéria de Qualidade e Testes de Software com base no desenvolvimento de uma calculadora simples. Foram implementados testes com JUnit. A classe Calculadora foi documentada com javadoc.
+
 ## Build e execução dos testes unitários
 
 ```
@@ -9,3 +11,9 @@ mvn package
 ## Exemplos de testes implementados
 
 <img width="500px" src="https://raw.githubusercontent.com/MatthMiller/calculadora-junit/c64a75c4089294632bfdc9fa893c9d702c58a08c/public/junit-exemplos.jpg" alt="Códigos de exemplo" />
+
+## Imagem da documentação em Javadoc
+
+HTML do Javadoc pode ser acessado em `target/site/apidocs/com/calculadora/app/Calculadora.html`.
+
+<img src="/public/javadoc.jpg" alt="Parte do javadoc" />
--- src/main/java/com/calculadora/app/Calculadora.java
+++ src/main/java/com/calculadora/app/Calculadora.java
@@ -1,23 +1,49 @@
 package com.calculadora.app;
 
-// Classe calculadora
+/**
+ * Classe calculadora que realiza quatro operações aritméticas: soma, subtração, multiplicação e divisão.
+ * @author Matheus Monteiro Bueno
+ * @version 1.3
+ * @since Release 1.0 da aplicação (Sep 22 2023).
+ */
 public class Calculadora {
-    // Método somar, aceita dois argumentos como double e retorna sua soma
+    /**
+     * Método de adição.
+     * @param primeiroNumero Primeiro valor da soma.
+     * @param segundoNumero Segundo valor da soma.
+     * @return Retorna a soma dos dois argumentos.
+     */
     public static double somar(double primeiroNumero, double segundoNumero) {
         return primeiroNumero + segundoNumero;
     }
 
-    // Método subtrair, aceita dois argumentos como double e retorna sua subtração
+    /**
+     * Método de subtração.
+     * @param primeiroNumero Minuendo, o valor que vai sofrer a subtração.
+     * @param segundoNumero Subtraendo, o valor que vai subtrair.
+     * @return Retorna a diferença entre os argumentos.
+     */
     public static double subtrair(double primeiroNumero, double segundoNumero) {
         return primeiroNumero - segundoNumero;
     }
 
-    // Método multiplicar, aceita dois argumentos como double e retorna sua multiplicação
+    /**
+     * Método de multiplicação.
+     * @param primeiroNumero Primeiro fator da multiplicação.
+     * @param segundoNumero Segundo fator da multiplicação.
+     * @return Retorna o resultado da multiplicação.
+     */
     public static double multiplicar(double primeiroNumero, double segundoNumero) {
         return primeiroNumero * segundoNumero;
     }
 
-    // Método dividir, aceita dois argumentos como double e retorna sua divisão
+     /**
+     * Método que realiza a divisão.
+     * @param dividendo Dividendo da operação de divisão.
+     * @param divisor Divisor da operação de divisão.
+     * @return Retorna o resultado da divisão.
+     * @throws IllegalArgumentException Se o divisor informado for 0.
+     */
     public static double dividir(double dividendo, double divisor) {
         // Caso seja feita a divisão por 0, uma exceção é lançada
         if (divisor == 0) {
